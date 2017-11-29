92195222dad22b52cf679b53199e781627b18fee
DaniGlass <danielleglass@me.com>
2017-11-29 12:22:10 -0800

    set debug to true

--- cafesite/settings.py
+++ cafesite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0noukj7%yr+=b*q)42@^9p+dv43g$&utz6z(gddiai11pya6ns'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 

e0de193eecd87ed4056c1756ba2c1ac792629168
DaniGlass <danielleglass@me.com>
2017-11-28 20:38:46 -0800

    correct db issue

--- cafesite/settings.py
+++ cafesite/settings.py
@@ -76,9 +76,10 @@ WSGI_APPLICATION = 'cafesite.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
